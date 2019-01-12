44f93650f6367ea15cc0252f116697a2cf168e99
terabytesoftw <terabytesoftw@gmail.com>
2019-01-12 18:07:53 -0400

    Update composer.json and config - (#1)

--- config/messages.php
+++ config/messages.php
@@ -14,12 +14,12 @@ return [
     'sourcePath' => '@app',
     // array, required, list of language codes that the extracted messages
     // should be translated to. For example, ['zh-CN', 'de'].
-    'languages' => ['en'],
+    'languages' => ['es', 'ru', 'fr', 'pt'],
     // string, the name of the function for translating messages.
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
     // multiple function names.
-    'translator' => '$this->app->t',
+    'translator' => '$this->getApp()->t',
     // boolean, whether to sort messages by keys when merging new messages
     // with the existing ones. Defaults to false, which means the new (untranslated)
     // messages will be separated from the old (translated) ones.
--- config/params.php
+++ config/params.php
@@ -8,16 +8,10 @@ return [
 	'app.name' => 'My Project Basic',
     'adminEmail' => 'admin@example.com',
     'debug.allowedIPs' => ['127.0.0.1', '::1'],
+    'debug.enable' => false,
     'favicon.ico' => '@yii/app/../public/favicon.ico',
-    'user.passwordResetTokenExpire' => 3600,
-    // database:
-    'db.dsn' => 'mysql:host=localhost;dbname=your_database;charset=utf8',
-	'db.username' => 'your_username',
-    'db.password' => 'your_password',
     // mailer:
 	'mailer.useFileTransport' => true,
     // translator:
 	'i18n.locale' => 'en',
-    'translator.basePath' => dirname(__DIR__) . '/messages',
-    'translator.sourceLanguage' => 'en',
 ];
