73d969257d44af6f366a2fd8d40e1b21a8d5f019
terabytesoftw <terabytesoftw@gmail.com>
2019-01-14 08:29:26 -0400

    Update config composer.json, messages.php - (#1).

--- config/messages.php
+++ config/messages.php
@@ -19,7 +19,7 @@ return [
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
     // multiple function names.
-    'translator' => '$this->getApp()->t',
+    'translator' => ['Yii::t', '$this->getApp()->t'],
     // boolean, whether to sort messages by keys when merging new messages
     // with the existing ones. Defaults to false, which means the new (untranslated)
     // messages will be separated from the old (translated) ones.
