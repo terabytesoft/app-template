37b0f3c370fcc15fcc3b6818c9e56b118e1cf9eb
terabytesoftw <terabytesoftw@gmail.com>
2019-05-20 13:53:58 -0400

    Fix common config

--- config/common.php
+++ config/common.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    'cache' => [
+        '__class' => \Yiisoft\Cache\Cache::class,
+        'handler' => [
+            '__class'   => \Yiisoft\Cache\FileCache::class,
+            'keyPrefix' => 'AppBasic'
+        ],
+    ],
     'logger' => function () {
         return new \Yiisoft\Log\Logger([
             new Yiisoft\Log\FileTarget(
